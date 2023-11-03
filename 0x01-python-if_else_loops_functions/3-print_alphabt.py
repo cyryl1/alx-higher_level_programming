eabf20206f9e0536bf79ce9763c1d6066dc086bb
Praise Aribisala <aribisalapraise12@gmail.com>
2023-11-03 17:06:58 +0100

    Commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(ord('a'), ord('z') + 1):
-    if i == q or i == e:
-        continue
-print("{}".format(chr(i)), end="")
+    if i == 'q' and i == 'e':
+        break
+    print("{}".format(chr(i)), end="")
