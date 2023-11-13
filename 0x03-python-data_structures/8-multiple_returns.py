d7d48bc395cce08b1a022d9deccc9f9056334f6c
Praise Aribisala <aribisalapraise12@gmail.com>
2023-11-13 08:58:51 +0100

    Commit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        return None
     length = len(sentence)
     first = sentence[0]
+    if sentence == "":
+        first == None
     return length, first
