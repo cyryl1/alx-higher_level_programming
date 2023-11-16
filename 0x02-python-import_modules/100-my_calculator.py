f879debde388d21bae1a9b57b602ab9438195f68
Praise Aribisala <aribisalapraise12@gmail.com>
2023-11-16 10:27:57 +0100

    Commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,5 +15,5 @@ if __name__ = "__main__":
         sys.exit(1)
 
     a = int(sys.argv[1])
-    b = int(sys.arv[3])
+    b = int(sys.argv[3])
     print("{} {} {} = {}".format(a, sys.argv[2], b, operator[sys.argv[2]](a, b)))
