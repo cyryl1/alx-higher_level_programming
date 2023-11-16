4ae7e8dc36bd9ec4c10e46b114b6e5bc6fea8e9c
Praise Aribisala <aribisalapraise12@gmail.com>
2023-11-16 10:02:04 +0100

    Commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
+if __name__ = "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
@@ -8,13 +8,12 @@ if __name__ == "__main__":
     if args != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
+
     operator = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(operator.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
-    b = int(sys.argv[3])
-
+    b = int(sys.arv[3])
     print("{} {} {} = {}".format(a, sys.argv[2], b, operator[sys.argv[2]](a, b)))
