276c79ab418118aeea4395e479ec2a445aedc5cf
Praise Aribisala <aribisalapraise12@gmail.com>
2023-11-23 09:54:39 +0100

    Commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 
 def best_score(a_dictionary):
     if a_dictionary == None or len(a_dictionary) == 0:
-        return None
+        return (None)
     max_key = max(a_dictionary, key = lambda k: a_dictionary[k])
     return (max_key)
