06afe6e6c69b53436a5222650396f058b6344c82
Praise Aribisala <aribisalapraise12@gmail.com>
2023-12-01 05:58:43 +0100

    Commit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    new_dict = {key: value * 2 for key, value in a_dictionary.items()}
+    new_dict = {key: a_dictionary[key] * 2 for key in a_dictionary}
     return (new_dict)
