f37c7299df8916605d122b499824185bc4ed4411
Praise Aribisala <aribisalapraise12@gmail.com>
2023-12-04 10:34:39 +0100

    Commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""Define Square"""
 
 class Square:
+    """Represent square"""
+
     def __init__(self, size):
+        """Initialize square.
+
+        Args:
+            size (int): size of square.
+        """
         self.__size = size
