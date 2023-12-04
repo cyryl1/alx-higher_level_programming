8033a69b88c61568da75f5e6b5ea5fecb312a8f8
Praise Aribisala <aribisalapraise12@gmail.com>
2023-12-04 10:47:58 +0100

    Commit

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""Square class definition."""
+class Square:
+    """Represent a square."""
+    def __init__(self, size=0):
+        """Square Class initialization.
+
+        Args:
+            size (int): Size of Square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/set the size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return area"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        """Print the square to stdout using #."""
+        for _ in range(self.__size):
+            for _ in range(self.__size):
+                print("#", end=" ")
+            print()
