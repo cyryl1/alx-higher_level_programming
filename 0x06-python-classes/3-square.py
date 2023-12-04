754cb6982532ce5e093912125ac458109ff321b9
Praise Aribisala <aribisalapraise12@gmail.com>
2023-12-04 10:30:11 +0100

    Commit

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""Square class Definition."""
 
 class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
+        """Square class initialization.
+
+        Args:
+            size (int): The size of the new square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -9,4 +17,5 @@ class Square:
         self.__size = size
 
     def area(self):
+        """Return area."""
         return (self.__size * self.__size)             
