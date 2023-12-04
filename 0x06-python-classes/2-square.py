515de3eef87b7cf2d4d062c7e255faf3acc702cd
Praise Aribisala <aribisalapraise12@gmail.com>
2023-12-04 10:32:44 +0100

    Commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""Class Square definition."""
 
 class Square:
+    """Class Square representation."""
+
     def __init__(self, size=0):
+        """Class intialization.
+
+        Args:
+            size (int): size of the square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
