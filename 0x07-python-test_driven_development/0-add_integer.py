ae99bf0308ca31000170d0ef57b2357646c299ce
Praise Aribisala <aribisalapraise12@gmail.com>
2023-12-05 07:19:07 +0100

    Commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines an function that adds two integers."""
+
+def add_integer(a, b=98):
+    """Returns the addition of a and b.
+
+    Floats are converted to int before addition is performed.
+
+    Raises:
+        TypeError; if either of a or b is a non-integer and non-float.
+    """
+    if (not isinstance(a, int) and not isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if (not isinstance(b, int) and not isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
