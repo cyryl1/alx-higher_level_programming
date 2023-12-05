df82e4e14d6272e2f9fe0bce19ba71d9fbac1a47
Praise Aribisala <aribisalapraise12@gmail.com>
2023-12-05 09:18:06 +0100

    Commit

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define function to indent"""
+
+def text_indentation(text):
+    """
+    Print a modified version of the provided text. Inserts 2 new lines after each occurrence of
+    '.', '?', and ':'.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        None
+
+    Raises:
+        TypeError: If the provided text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string.")
+
+    for char in ['.', '?', ':']:
+        text = text.replace(char, f"{char}\n\n")
+    print(text)
