94bc2c1fd392f2affc65b107201e91c5e68bf6fe
Praise Aribisala <aribisalapraise12@gmail.com>
2024-01-04 07:30:34 +0100

    New commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines class BaseGeometry."""
+
+
+class BaseGeometry:
+    """To calculate the base geometry of a circle."""
+    def area(self):
+        """Function to raise an Exception."""
+        raise Exception("area() is not implemented")
