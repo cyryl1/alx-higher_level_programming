e76aea6ea4e94ce3b3abe5d3142330714ebafdac
Praise Aribisala <aribisalapraise12@gmail.com>
2024-01-04 13:16:24 +0100

    New commit

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -13,7 +13,7 @@ class MyInt(int):
     def __eq__(self, value):
         """return false if == operator."""
         return (self.real != value)
-    
+
     def __ne__(self, value):
         """returns true if != operator."""
         return (self.real == value)
