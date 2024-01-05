17111e29cbf33ebcc86a74ac230146cef8d74ae3
Praise Aribisala <aribisalapraise12@gmail.com>
2024-01-05 10:58:00 +0100

    New commit

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = sys.argv
+filename = "add_item.json"
+with open(filename, 'a+', encoding="utf-8") as f:
+    List = []
+    List.extend(args[1:])
+    save_to_json_file(List, filename)
+    load_from_json_file(filename)
