ca7de52881c450aa4b914d18633a6ae53e704d98
Praise Aribisala <aribisalapraise12@gmail.com>
2024-01-07 15:24:49 +0100

    New commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -34,7 +34,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        if isinstance(value, int) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -47,7 +47,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        if isinstance(value, int) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -60,7 +60,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if type(value) != int:
+        if isinstance(value, int) is not int:
             raise TypeError("x must be an integer")
         if value <= 0:
             raise ValueError("x must be >= 0")
@@ -73,7 +73,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if type(value) != int:
+        if isinstance(value, int) is not int:
             raise TypeError("y must be an integer")
         if value <= 0:
             raise ValueError("y must be >= 0")
@@ -154,4 +154,4 @@ class Rectangle(Base):
     def __str__(self):
         """Return the print() and str() representation of the Rectangle."""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
-                self.width, self.height)
+                 self.width, self.height)
