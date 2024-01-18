daa12adbca35c84daa4f95d79edc99061e509a39
Praise Aribisala <aribisalapraise12@gmail.com>
2024-01-18 15:17:08 +0100

    New commit

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked.
-SELECT t.title, IFNULL(g.genre_id, NULL)
+SELECT t.title, IFNULL(g.genre_id, NULL) AS genre_id
         FROM tv_shows AS t
         LEFT JOIN tv_show_genres AS g
         ON g.show_id=t.id
