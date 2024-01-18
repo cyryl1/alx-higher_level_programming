ce74335c5a747d023e4d75c92487f8147991b2a0
Praise Aribisala <aribisalapraise12@gmail.com>
2024-01-18 12:40:56 +0100

    New commit

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT t.title, g.genre_id
+  	FROM tv_shows AS t
+        INNER JOIN tv_show_genres AS g
+        ON g.genre_id=t.id
+ORDER BY t.title, g.genre_id ASC;
