77f0646a7af844ce68e2e75ffe3dbb360c6fcbde
Praise Aribisala <aribisalapraise12@gmail.com>
2024-02-12 16:59:00 +0100

    Edited task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,7 +4,7 @@ const process = require('process');
 const args = process.argv;
 
 if (args.length <= 2) {
-  console.log('No arguments');
+  console.log('No argument');
 } else if (args.length === 3) {
   console.log('Argument found');
 } else {
