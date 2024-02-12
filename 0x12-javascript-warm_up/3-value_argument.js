243225c679f2bdf355f04c6c9c1f1d5c9f7a8841
Praise Aribisala <aribisalapraise12@gmail.com>
2024-02-12 17:01:31 +0100

    Edited task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,7 +4,7 @@ const process = require('process');
 const args = process.argv;
 
 if (!args[2]) {
-  console.log('No arguments');
+  console.log('No argument');
 } else {
   console.log(args[2]);
 }
