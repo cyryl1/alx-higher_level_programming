0f3487cc263fccd4fc4cc4837fe295e07aeae685
Praise Aribisala <aribisalapraise12@gmail.com>
2024-02-19 03:27:12 +0100

    Edit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,6 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
     db = MySQLdb.connect(
             host="localhost",
