b415b111d7975a479695fa08dea000175219ee47
Praise Aribisala <aribisalapraise12@gmail.com>
2024-02-19 03:46:16 +0100

    Edit

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,43 +1,14 @@
 #!/usr/bin/python3
-import MySQLdb
-"""Defines a database"""
+# Lists all atates from the database hbtn_0e_0_usa.
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
+
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    """ Check if the correct number of command-line arguments is provided"""
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    """Extract MySQL credentials from command-lne arguments"""
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    """Establish a connection to the MySQL server"""
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-    )
-    """The cursor object to interact with the database"""
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-
-    """
-        Executes the SQL query to retrieve the
-        states with names starting from N
-    """
-    query = cur.execute(
-            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    )
-
-    """Fetches all the rows"""
-    all_states = cur.fetchall()
-
-    """Display result"""
-    for state in all_states:
-        print(state)
-
-    """Closes the cursor and databse connection"""
-    cur.close()
-    db.close()
+    cur.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in cur.fetchall() if state[1][0] == "N"]
