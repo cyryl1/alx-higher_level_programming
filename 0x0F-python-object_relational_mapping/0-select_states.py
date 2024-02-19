a15cbba47a6eef35b6a71c1646f6255ad478acd5
Praise Aribisala <aribisalapraise12@gmail.com>
2024-02-19 03:21:10 +0100

    Task 0 edited

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-import MySQLdb
-"""Defines a database"""
+# Lists all atates from the database hbtn_0e_0_usa.
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
+
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    """ Check if the correct number of command-line arguments is provided"""
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
-    """Extract MySQL credentials from command-lne arguments"""
+    #Extract MySQL credentials from command-lne arguments
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    """Establish a connection to the MySQL server"""
+    #Establish a connection to the MySQL server
     db = MySQLdb.connect(
             host="localhost",
             port=3306,
@@ -21,19 +24,18 @@ if __name__ == "__main__":
             db=database
     )
 
-    """The cursor object to interact with the database"""
+    #The cursor object to interact with the database
     cur = db.cursor()
 
-    """Executes the SQL query to retrieve the states"""
+    #Executes the SQL query to retrieve the states
     query = cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    """Fetches all the rows"""
+    #Fetches all the rows
     all_states = cur.fetchall()
 
-    """Display result"""
-    for state in all_states:
-        print(state)
+    #Display result
+    [print(state) for state in all_states]
 
-    """Closes the cursor and databse connection"""
+    #Closes the cursor and databse connection
     cur.close()
     db.close()
