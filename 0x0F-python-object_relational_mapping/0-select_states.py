87924587e33f0f1a50a2f882d57dd46215f64ac2
Praise Aribisala <aribisalapraise12@gmail.com>
2024-02-19 03:24:56 +0100

    Edit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,11 +11,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
-
-    #Extract MySQL credentials from command-lne arguments
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    #Establish a connection to the MySQL server
     db = MySQLdb.connect(
             host="localhost",
             port=3306,
@@ -23,19 +19,8 @@ if __name__ == "__main__":
             passwd=password,
             db=database
     )
-
-    #The cursor object to interact with the database
     cur = db.cursor()
-
-    #Executes the SQL query to retrieve the states
-    query = cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    #Fetches all the rows
-    all_states = cur.fetchall()
-
-    #Display result
-    [print(state) for state in all_states]
-
-    #Closes the cursor and databse connection
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    [print(state) for state in cur.fetchall()]
     cur.close()
     db.close()
