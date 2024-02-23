2b3d1867fbc80cae0270fbb0fab76f0ce3f193ae
Praise Aribisala <aribisalapraise12@gmail.com>
2024-02-23 05:54:19 +0100

    Task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -6,9 +6,9 @@ To print given state object in the database
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
-def state_obj(user, password, database, state):
+def state_obj(user, password, database, state_name):
      engine = create_engine(
             "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
                 user, password, database
@@ -16,19 +16,16 @@ def state_obj(user, password, database, state):
             )
      Session = sessionmaker(bind=engine)
      session = Session()
-     obj = 0
-     for states in session.query(State).order_by(State.id):
-         if state == states
-             obj += 1
-         else:
-             print("Not found")
-     print(obj)
+
+     is_state = False
+     for state in session.query(State):
+         if state.name == state_name:
+             print("{}".format(state.id))
+             is_state = True
+             break
+     if is_state is False:
+         print("Not found")
 
 
 if __name__ == "__main__":
-     user = sys.argv[1]
-     password = sys.argv[2]
-     database = sys.argv[3]
-     state = sys.argv[4]
-
-     state_obj(user, password, database, state)
+     state_obj(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
