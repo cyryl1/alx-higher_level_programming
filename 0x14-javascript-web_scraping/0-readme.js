b890ee96c46f8d9fc59035841378823a917c7708
Praise Aribisala <aribisalapraise12@gmail.com>
2024-07-31 06:38:39 +0100

    Task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const args = process.argv;
+const file = args[2];
+
+if (!file) {
+	console.error('Usage: ./script.js <filename>');
+	process.exit(1);
+}
+
+fs.readFile(file, 'utf-8',  (error, data) => {
+	if (error) {
+		console.error('error:', error);
+	} else {
+		console.log(data);
+	}
+});
+
