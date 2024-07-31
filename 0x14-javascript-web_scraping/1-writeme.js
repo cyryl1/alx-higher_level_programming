6fbe0554a515cbc778c5d086ad4bcb8e8bf097d0
Praise Aribisala <aribisalapraise12@gmail.com>
2024-07-31 06:58:34 +0100

    Task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const args = process.argv;
+const file = args[2];
+const content = args[3];
+
+if (!file || !content) {
+	console.log("Usage: ./script.js <filename> <content>");
+	process.exit(1);
+}
+
+fs.writeFile(file, content, 'utf-8', (error) => {
+
+	if (error)
+		console.error('error: ', error);
+});
